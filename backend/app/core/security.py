b9bfb70fc4585ae6a19f658b4d78af412931565b
Dylan <dylanmckay2004@gmail.com>
2026-04-24 17:09:42 -0400

    Fix synax error (missing semicolon)

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -11,7 +11,7 @@ SOCKET_TOKEN_EXPIRE_SECONDS = 60
 pwd_context = CryptContext(schemes=["bcrypt_sha256"], deprecated="auto")
 
 
-def hash_password(password str) -> str:
+def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
 
